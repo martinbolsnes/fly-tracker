606788f7d14b0299ab0f28fcdbc7c7c3d9d9be4b
martinbolsnes <martinbolsones@gmail.com>
2025-01-15 19:43:08 +0100

    Add Supabase client and middleware for session management and cookie handling

---
+++ lib/supabase/middleware.ts
@@ -0,0 +1,64 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { NextResponse, type NextRequest } from 'next/server';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export async function updateSession(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(supabaseUrl, supabaseKey, {
+    cookies: {
+      get(name: string) {
+        return request.cookies.get(name)?.value;
+      },
+      set(name: string, value: string, options: CookieOptions) {
+        request.cookies.set({
+          name,
+          value,
+          ...options,
+        });
+        response = NextResponse.next({
+          request: {
+            headers: request.headers,
+          },
+        });
+        response.cookies.set({
+          name,
+          value,
+          ...options,
+        });
+      },
+      remove(name: string, options: CookieOptions) {
+        request.cookies.set({
+          name,
+          value: '',
+          ...options,
+        });
+        response = NextResponse.next({
+          request: {
+            headers: request.headers,
+          },
+        });
+        response.cookies.set({
+          name,
+          value: '',
+          ...options,
+        });
+      },
+    },
+  });
+
+  // refreshing the auth token
+  const user = await supabase.auth.getUser();
+
+  if (request.nextUrl.pathname.startsWith('/profile') && user.error) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return response;
+}
