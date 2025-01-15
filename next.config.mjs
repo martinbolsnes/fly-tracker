9c690c98b11b6bf24b892a655193e84a53518dcd
martinbolsnes <martinbolsones@gmail.com>
2025-01-15 19:39:46 +0100

    Add configuration for Tailwind CSS and image domains in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['pbdjqozuekzpfhyoyjiw.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbdjqozuekzpfhyoyjiw.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/trip-images/',
+        search: '',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
