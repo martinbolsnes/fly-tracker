b1b56fd3c7fd2f750be705ead52c9aa6c812d87c
martinbolsnes <martinbolsones@gmail.com>
2025-01-17 23:18:26 +0100

    Remove obsolete login components and error page

--- app/components/Login/(login)/login/error/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ErrorPage() {
-  return (
-    <div className='flex flex-col items-center justify-center space-y-4'>
-      <h1 className='text-lg font-semibold'>Oops! 😬</h1>
-      <p className='text-foreground'>Noe gikk galt, prøv igjen.</p>
-    </div>
-  );
-}
