14e8f0fe333819ea1a73671a4cdfe6ce40c12242
martinbolsnes <martinbolsones@gmail.com>
2025-01-17 23:14:16 +0100

    Update error page text for improved user experience

--- app/(login)/login/error/page.tsx
+++ app/(login)/login/error/page.tsx
@@ -1,8 +1,8 @@
 export default function ErrorPage() {
   return (
     <div className='flex flex-col items-center justify-center space-y-4'>
-      <h1 className='text-lg font-semibold'>Oops! 😬</h1>
-      <p className='text-foreground'>Noe gikk galt, prøv igjen.</p>
+      <h1 className='text-lg font-semibold'>Were're sorry 😬</h1>
+      <p className='text-foreground'>Something went wrong...</p>
     </div>
   );
 }
