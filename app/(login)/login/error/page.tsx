c0e70404711cf8db7e4152ac716ca4b03d343109
martinbolsnes <martinbolsones@gmail.com>
2025-01-23 12:13:14 +0100

    Add navigation button to error page for better user experience

--- app/(login)/login/error/page.tsx
+++ app/(login)/login/error/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function ErrorPage() {
   return (
     <div className='flex flex-col items-center justify-center space-y-4'>
       <h1 className='text-lg font-semibold'>Oops... 😬</h1>
       <p className='text-foreground'>Something went wrong</p>
+      <Link href='/'>
+        <Button variant='outline'>Go back home</Button>
+      </Link>
     </div>
   );
 }
